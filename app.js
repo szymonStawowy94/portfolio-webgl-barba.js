97aea68d447e73120c20b2006f5c8ac9026e3725
Szymon <s.stawowy@pleodigital.com>
2023-10-10 08:37:40 +0200

    sync-html-and-webgl-dimensions (DO NOT MERGE TO MAIN)

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ export default class Sketch{
         this.container = options.domElement;
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
-        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera = new THREE.PerspectiveCamera( 30, this.width / this.height, 10, 1000 );
+        this.camera.position.z = 600;
+
+        this.camera.fov = 2*Math.atan((this.height / 2) / 600 ) * 180/Math.PI;
         this.renderer = new THREE.WebGLRenderer( {
             antialias: true, alpha: true
         } );
@@ -43,8 +45,8 @@ export default class Sketch{
 
     addObjects() {
         // this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        // this.geometry = new THREE.PlaneGeometry( 0.5, 0.5, 100, 100 );
-        this.geometry = new THREE.SphereGeometry( 0.5, 100, 100 );
+        this.geometry = new THREE.PlaneGeometry( 350, 350, 100, 100 );
+        // this.geometry = new THREE.SphereGeometry( 0.5, 100, 100 );
         console.log(this.geometry);
         // this.geometry = new THREE.SphereGeometry( 0.2, 30,  30 );
         // this.material = new THREE.MeshNormalMaterial();
