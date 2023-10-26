d1d1f0fb9fbd78c3f722e637c4eba7cb943914a1
Szymon <s.stawowy@pleodigital.com>
2023-10-26 08:24:36 +0200

    barba.js portfolio

--- dist/inside.js
+++ dist/inside.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63648" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -338,4 +338,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../Users/sstaw/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=/inside.js.map
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,26 +3,29 @@ uniform float uProgress;
 uniform vec2 uResolution;
 uniform vec2 uQuadSize;
 uniform vec4 uCorners;
-varying vec2 vUv;
 varying vec2 vSize;
+
+varying vec2 vUv;
 void main() {
-    vUv = uv;
     float PI = 3.1415926;
+    vUv = uv;
     float sine = sin(PI*uProgress);
-    float waves = sine*0.1*sin(5.*length(uv)+ 5.*uProgress);
-    vec4 defaultState = modelMatrix * vec4(position, 1.0);
-    vec4 fullScreenState = vec4(position, 1.0);
-
+    float waves = sine*0.1*sin(5.*length(uv) + 15.*uProgress);
+    vec4 defaultState = modelMatrix*vec4( position, 1.0 );
+    vec4 fullScreenState = vec4( position, 1.0 );
+    fullScreenState.x *=uResolution.x;
+    fullScreenState.y *=uResolution.y;
+    fullScreenState.z +=uCorners.x;
     float cornersProgress = mix(
-        mix(uCorners.x,uCorners.y, uv.x),
-        mix(uCorners.z,uCorners.w, uv.x),
-        uv.y
+    mix(uCorners.z,uCorners.w,uv.x),
+    mix(uCorners.x,uCorners.y,uv.x),
+    uv.y
     );
-    vec4 finalState = mix(defaultState, fullScreenState, uProgress + waves);
 
-    vSize = mix(uQuadSize, uResolution, uProgress);
 
-//    fullScreenState.x *=uResolution.x/uQuadSize.x;
-//    fullScreenState.y *=uResolution.y/uQuadSize.y;
+    vec4 finalState = mix(defaultState,fullScreenState,cornersProgress);
+
+    vSize = mix(uQuadSize,uResolution,cornersProgress);
+
     gl_Position = projectionMatrix * viewMatrix * finalState;
 }
